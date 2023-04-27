7115be3ce25e4928f83c5ea0cb34a1f500999f5f
elhamzeinodini <elhammth@gmail.com>
2023-04-27 15:55:28 +0330

    update script2

--- build/js/script2.js
+++ build/js/script2.js
@@ -13,3 +13,25 @@ log(findMultiplyByTwo);
 // to be returned ,and you want to perform operation on all elements,
 // then you can use the filter() method. But if you expect only a single
 // element to be returned from the array, then you can use find() and avoid extra iterations.
+// comparison
+const item1 = true;
+const item2 = "true";
+// log(item1 === item2)  // false
+// log(item1 == item2)   // false
+// working with dates
+const item3 = new Date();
+const item4 = item3.toString();
+const item5 = item3.toISOString();
+const item6 = item3.getDate();
+console.log(item4);
+console.log(item5);
+console.log(item6);
+// convert string to number
+const item7 = "12345";
+const item8 = +item7;
+console.log(item8, typeof item8);
+const item9 = "1234";
+const item10 = Number(item9);
+console.log(item10, typeof item10);
+const item11 = parseInt(item9);
+console.log(item11, typeof item11);
--- src/script2.ts
+++ src/script2.ts
@@ -17,3 +17,29 @@ log(findMultiplyByTwo)
 // then you can use the filter() method. But if you expect only a single
 // element to be returned from the array, then you can use find() and avoid extra iterations.
 
+// comparison
+const item1: boolean = true
+const item2: string = "true"
+// log(item1 === item2)  // false
+// log(item1 == item2)   // false
+
+// working with dates
+const item3: Date = new Date()
+const item4: string = item3.toString()
+const item5: string = item3.toISOString()
+const item6: number = item3.getDate()
+console.log(item4)
+console.log(item5)
+console.log(item6)
+
+// convert string to number
+const item7:string = "12345"
+const item8:number = +item7
+console.log(item8, typeof item8)
+
+const item9:string = "1234"
+const item10:number = Number(item9)
+console.log(item10, typeof item10)
+
+const item11:number = parseInt(item9)
+console.log(item11, typeof item11)
